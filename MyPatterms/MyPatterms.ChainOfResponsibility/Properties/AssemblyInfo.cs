612362cf87c802320f461cca81bad0e723f35cee
张宙哲 <zhangzhouzhe@BS-BJB0915>
2020-12-11 16:44:38 +0800

    添加责任链模式

---
+++ MyPatterms/MyPatterms.ChainOfResponsibility/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyPatterms.ChainOfResponsibility")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyPatterms.ChainOfResponsibility")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3608778-214f-488d-b98c-64804fb63886")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyPatterms/MyPatterms.ChainOfResponsibility/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyPatterms.ChainOfResponsibility
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var request = new PurchaseRequest()
+            {
+                Amount = 10
+            };
+
+            var manager = new Manager(new VP(null));
+
+            manager.ProcessRequest(request);
+
+            request.Amount = 900;
+
+            manager.ProcessRequest(request);
+        }
+    }
+
+
+
+    public class PurchaseRequest
+    {
+        public int Amount { get; set; }
+    }
+
+    public abstract class Approver
+    {
+        protected Approver _nextApprover;
+
+        public string Name { get; set; }
+
+        public abstract void ProcessRequest(PurchaseRequest request);
+    }
+    public class VP : Approver
+    {
+        public VP(Approver nextApprover)
+        {
+            base._nextApprover = nextApprover;
+            base.Name = "VP";
+        }
+        public override void ProcessRequest(PurchaseRequest request)
+        {
+            Console.WriteLine($"{Name}:开始处理");
+            if (request.Amount < 1000)
+            {
+                Console.WriteLine($"{Name}:已处理");
+            }
+            else
+            {
+                Console.WriteLine($"{Name}:无法处理");
+
+            }
+
+            Console.WriteLine($"{Name}:结束处理");
+        }
+    }
+    public class Manager : Approver
+    {
+        public override void ProcessRequest(PurchaseRequest request)
+        {
+            Console.WriteLine($"{Name}:开始处理");
+            if (request.Amount < 100)
+            {
+                Console.WriteLine($"{Name}:已处理");
+            }
+            else
+            {
+                Console.WriteLine($"{Name}:转交处理");
+                _nextApprover.ProcessRequest(request);
+            }
+
+            Console.WriteLine($"{Name}:结束处理");
+        }
+
+        public Manager(Approver nextApprover)
+        {
+            base._nextApprover = nextApprover;
+            base.Name = "经理";
+        }
+    }
+
+}
