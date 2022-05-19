943ea796c0e757106cc53e0ef6ae211da8e0a565
zhangzhouzhe <zhangzhouzhe@beisen.com>
2022-05-19 22:01:00 +0800

    Decorator

---
+++ MyPatterms/MyPatterms.Decorator/UnitTest1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MyPatterms.Decorator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+
+    public abstract class Food
+    {
+        protected string desc;
+        public abstract string GetDesc();
+    }
+    public class Chicken : Food
+    {
+        public Chicken()
+        {
+            desc = "Chicken";
+        }
+        public override string GetDesc()
+        {
+            return desc;
+        }
+    }
+    public class Duck : Food
+    {
+        public Duck()
+        {
+            desc = "Duck";
+        }
+        public override string GetDesc()
+        {
+            return desc;
+        }
+    }
+
+    public abstract class FoodDecorator : Food
+    {
+
+    }
+
+    public class RoastFood : FoodDecorator
+    {
+        private Food _food;
+        public RoastFood(Food food)
+        {
+            _food = food;
+        }
+        public override string GetDesc()
+        {
+            return "烤" + _food.GetDesc();
+        }
+    }
+    public class SteamedFood : FoodDecorator
+    {
+        private Food _food;
+        public SteamedFood(Food food)
+        {
+            _food = food;
+        }
+        public override string GetDesc()
+        {
+            return "蒸" + _food.GetDesc();
+        }
+    }
+}
